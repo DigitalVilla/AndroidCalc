6f2d79dffa4a24977646ed707fd2a6d462077593
Omar Villanueva Concha <767110@ACDM.DS.SAIT.CA>
2017-12-19 23:34:34 -0700

    Version 1.0

--- app/src/main/java/co/digitalvilla/mycalc/MainActivity.java
+++ app/src/main/java/co/digitalvilla/mycalc/MainActivity.java
@@ -14,29 +14,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // Link UI number buttons
-        Button btn1 = (Button) findViewById(R.id.button1);
-        Button btn2 = (Button) findViewById(R.id.button2);
-        Button btn3 = (Button) findViewById(R.id.button3);
-        Button btn4 = (Button) findViewById(R.id.button4);
-        Button btn5 = (Button) findViewById(R.id.button5);
-        Button btn6 = (Button) findViewById(R.id.button6);
-        Button btn7 = (Button) findViewById(R.id.button7);
-        Button btn8 = (Button) findViewById(R.id.button8);
-        Button btn9 = (Button) findViewById(R.id.button9);
-        Button btn0 = (Button) findViewById(R.id.button0);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        Button btn3 = (Button) findViewById(R.id.btn3);
+        Button btn4 = (Button) findViewById(R.id.btn4);
+        Button btn5 = (Button) findViewById(R.id.btn5);
+        Button btn6 = (Button) findViewById(R.id.btn6);
+        Button btn7 = (Button) findViewById(R.id.btn7);
+        Button btn8 = (Button) findViewById(R.id.btn8);
+        Button btn9 = (Button) findViewById(R.id.btn9);
+        Button btn0 = (Button) findViewById(R.id.btn0);
         // Link UI operation buttons
-        Button btnAc = (Button) findViewById(R.id.buttonClear);
-        Button btnDel = (Button) findViewById(R.id.buttonDelete);
-        Button btnMod = (Button) findViewById(R.id.buttonModulous);
-        Button btnCent = (Button) findViewById(R.id.buttonPercent);
-        Button btnDiv = (Button) findViewById(R.id.buttonDivide);
-        Button btnMulti = (Button) findViewById(R.id.buttonMultiply);
-        Button btnMinus = (Button) findViewById(R.id.buttonMinus);
-        Button btnPlus = (Button) findViewById(R.id.buttonPlus);
-        Button btnDot= (Button) findViewById(R.id.buttonDecimal);
-        Button btnNeg = (Button) findViewById(R.id.buttonNegative);
+        Button btnAc = (Button) findViewById(R.id.btnAc);
+        Button btnDel = (Button) findViewById(R.id.btnDel);
+        Button btnMod = (Button) findViewById(R.id.btnMod);
+        Button btnCent = (Button) findViewById(R.id.btnCent);
+        Button btnDiv = (Button) findViewById(R.id.btnDiv);
+        Button btnMulti = (Button) findViewById(R.id.btnMulti);
+        Button btnMinus = (Button) findViewById(R.id.btnMinus);
+        Button btnPlus = (Button) findViewById(R.id.btnPlus);
+        Button btnDot= (Button) findViewById(R.id.btnDot);
+        Button btnNeg = (Button) findViewById(R.id.btnNeg);
         // Link UI ImageButton
-        ImageButton btnCalc = (ImageButton) findViewById(R.id.imageButtonEquals);
+        ImageButton iBtnCalc = (ImageButton) findViewById(R.id.iBtnCalc);
         //Link Text Views to diplay
         TextView tvResult = (TextView) findViewById(R.id.textViewTotal);
         TextView tvDisplay = (TextView) findViewById(R.id.textViewInput);
